318d5b96259f640b18b28c9f39bcb6e6d3420fec
Hamna M.N. <hamnamn-cs22059@stu.kln.ac.lk>
2025-10-19 22:02:55 +0530

    Lab worksheet_04 - Q7,Q8,Q9,Q10

---
+++ src/LW_04/Q8.java
@@ -0,0 +1,19 @@
+package LW_04;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Q8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+
+        for(int i=1;i<11;i++){
+            System.out.println(n + " * "+ i + " = " + n*i);
+        }
+
+    }
+
+
+}
